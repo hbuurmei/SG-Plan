34875294354763f4e54d0cc97bbd66b9e989e2dc
hbuurmei <hugo.buurmeijer@gmail.com>
2024-03-07 18:03:05 -0800

    astar fix

--- astar.py
+++ astar.py
@@ -19,6 +19,16 @@ def bad_heuristic(node1, node2):
 def node_dist(node1, node2):
     return np.linalg.norm(node1.attributes.position - node2.attributes.position, ord=2)
 
+def get_info(goal_node, path_dict, cost_to_come):
+    # returns usable information from dictionary
+    path_list = [goal_node]
+    node = path_dict[goal_node]
+    while node != None:
+        path_list.insert(0, node)
+        node = path_dict[node]
+    
+    return path_list, cost_to_come[goal_node]
+
 def layer_astar(G, start_node, goal_node, heuristic):
     # convert input from node.id.value to node
     if isinstance(start_node, int):
@@ -48,14 +58,8 @@ def layer_astar(G, start_node, goal_node, heuristic):
                 priority = cost + heuristic(neighbor_node, goal_node)
                 Q.put(PrioritizedNode(neighbor_node, priority))
                 path_dict[neighbor_node] = curr_node
-
-    path_list = [goal_node]
-    node = path_dict[goal_node]
-    while node != None:
-        path_list.insert(0, node)
-        node = path_dict[node]
     
-    return path_list, cost_to_come[goal_node]
+    return goal_node, path_dict, cost_to_come
 
 def naive_place_to_room_astar(G, start_node, goal_room, heuristic):
     # convert input from node.id.value to node
@@ -69,9 +73,10 @@ def naive_place_to_room_astar(G, start_node, goal_room, heuristic):
     if goal_room.layer != 4:
         raise Exception("Goal node is not a room")
 
+    path_dict={}
+    cost_to_come={}
+
     Q = PriorityQueue() # min cost_to_come + heuristic_cost_to_go priority queue
-    path_dict = {} 
-    cost_to_come = {}
     Q.put(PrioritizedNode(start_node, 0.))
     path_dict[start_node] = None
     cost_to_come[start_node] = 0.
@@ -89,14 +94,32 @@ def naive_place_to_room_astar(G, start_node, goal_room, heuristic):
                 priority = cost + heuristic(neighbor_node, goal_room)
                 Q.put(PrioritizedNode(neighbor_node, priority))
                 path_dict[neighbor_node] = curr_node
-
-    path_list = [curr_node]
-    node = path_dict[curr_node]
-    while node != None:
-        path_list.insert(0, node)
-        node = path_dict[node]
     
-    return path_list, cost_to_come[curr_node]
+    return curr_node, path_dict, cost_to_come
+
+def closest_place_to_room_astar(G, start_node, goal_room, heuristic): # bad
+    # convert input from node.id.value to node
+    if isinstance(start_node, int):
+        start_node = G.get_node(start_node)
+    if isinstance(goal_room, int):
+        goal_room = G.get_node(goal_room)
+    
+    if start_node.layer != 3:
+        raise Exception("Start node is not a place")
+    if goal_room.layer != 4:
+        raise Exception("Goal node is not a room")
+
+    # get closest_goal_room_node
+    idx = 0
+    room_node_pos = np.zeros((len(goal_room.children()), 3), dtype=float)
+    values = np.zeros(len(goal_room.children()), dtype=int)
+    for value in goal_room.children():
+        room_node_pos[idx, :] = G.get_node(value).attributes.position
+        values[idx] = value
+        idx += 1
+    room_node_dists = np.linalg.norm(room_node_pos, ord=2, axis=1)
+    closest_room_node = G.get_node(values[np.argmin(room_node_dists)])
+    return layer_astar(G, start_node, closest_room_node, heuristic)
 
 
 def hierarchical_planner(G, start_node, goal_node, heuristic):
@@ -114,13 +137,16 @@ def hierarchical_planner(G, start_node, goal_node, heuristic):
     # Room level planning
     start_room = G.get_node(start_node.get_parent())
     goal_room = G.get_node(goal_node.get_parent())
-    room_path, _ = layer_astar(G, start_room, goal_room, heuristic)
+    room_path, _ = get_info(*layer_astar(G, start_room, goal_room, heuristic))
     # Room to Room planning
     total_path_list = [start_node]
     total_cost = 0.
-    for i in range(len(room_path) - 1):
+    for i in range(len(room_path)):
         curr_node = total_path_list.pop()
-        path_segment, segment_cost = naive_place_to_room_astar(G, curr_node, room_path[i + 1], heuristic)
+        if i == len(room_path) - 1:
+            path_segment, segment_cost = get_info(*layer_astar(G, curr_node, goal_node, heuristic))
+        else:
+            path_segment, segment_cost = get_info(*naive_place_to_room_astar(G, curr_node, room_path[i + 1], heuristic))
         total_path_list.extend(path_segment)
         total_cost += segment_cost
     return total_path_list, total_cost
@@ -153,7 +179,7 @@ if __name__ == "__main__":
     place2 = G.get_node(list(room_nodes[8].children())[10])
     print(place1, place2)
     time1 = time.time()
-    path_list, total_cost = layer_astar(G, place1, place2, node_dist)
+    path_list, total_cost = get_info(*layer_astar(G, place1, place2, node_dist))
     time2 = time.time()
     print(f"Compute time: {time2 - time1} sec")
     print(path_list)
@@ -166,7 +192,7 @@ if __name__ == "__main__":
     # place1 = G.get_node(list(room_nodes[3].children())[0])
     # place2 = G.get_node(list(room_nodes[8].children())[10])
     # time1 = time.time()
-    # path_list, total_cost = naive_place_to_room_astar(G, place1, room_nodes[8], node_dist)
+    # path_list, total_cost = get_info(*naive_place_to_room_astar(G, place1, room_nodes[8], node_dist))
     # time2 = time.time()
     # print(f"Compute time: {time2 - time1} sec")
     # print(path_list)
