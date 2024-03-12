a26198c2de0060a13f6bd4405fe15fe16ede8628
hbuurmei <hugo.buurmeijer@gmail.com>
2024-03-11 20:57:28 -0700

    Simplify prompt

--- query_llm.py
+++ query_llm.py
@@ -1,17 +1,7 @@
 from transformers import pipeline
 
 
-def query_model(question, model_name='EleutherAI/gpt-neo-2.7B'):
-    """
-    Queries an open-source LLM model and returns the response.
-
-    Parameters:
-    - question (str): The question or prompt to send to the model.
-    - model_name (str): Identifier for the model to use. Defaults to GPT-Neo 2.7B.
-
-    Returns:
-    - str: The model's response to the question.
-    """
+def query_model(question, model_name='mistralai/Mistral-7B-v0.1'):
     # Initialize the model and tokenizer
     generator = pipeline('text-generation', model=model_name)
 
@@ -26,6 +16,6 @@ objects = ["fan", "papers", "keyboard", "laptop", "mouse", "trashcan", "chair", 
 question = "I want to sit comfortably, where shall I go?"
 options_prompt = "Please choose from the following options: " + ", ".join(objects) + ". Please only type the name of the object."
 prompt = question + " " + options_prompt
-print(prompt)
+print("Prompt: ", prompt)
 # response = query_model(prompt, 'mistralai/Mistral-7B-v0.1')
-# print(response)
+# print("Response: ", response)
